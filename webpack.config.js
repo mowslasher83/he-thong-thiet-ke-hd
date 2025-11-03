a251e3bcc36f0209ba106a244b1bcd2e054ed809
mowslasher83 <radeswaled@hotmail.com>
2025-11-03 04:42:31 -0600

    Update webpack configuration for better readability.

--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,24 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js'
+    filename: 'bundle.[contenthash].js',
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/,  
         exclude: /node_modules/, 
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
-    ]
+    ],
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
-    hot: true
+    hot: true,
   },
 };
